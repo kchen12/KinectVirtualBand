7ccbb292d2ca33d3029696108019be4e1f5c6582
unknown <>
2016-10-30 16:01:02 -0400

    used vangos kinect-coordinate-mapping to setup color stream and joints

--- KinectVirtualBand/KinectVirtualBand/KinectVirtualBand/MainWindow.xaml.cs
+++ KinectVirtualBand/KinectVirtualBand/KinectVirtualBand/MainWindow.xaml.cs
@@ -1,30 +1,255 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Kinect;
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+// Based off of Vangos kinect-coordinate-mapping
 
 namespace KinectVirtualBand
 {
+    using System;
+    //using System.Drawing;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using System.Windows.Controls;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        private WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// Intermediate storage for the color data recieved from the camera
+        /// </summary>
+        private byte[] colorPixels;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+        Skeleton[] _bodies = new Skeleton[6];
+
+        CameraMode _mode = CameraMode.Color;
+
+        enum CameraMode
+        {
+            Color,
+            Depth
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
         public MainWindow()
         {
-            // figure out how to import library
-            private KinectSensor sensor;
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Execute startup tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                //this.sensor.DepthStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.AllFramesReady += this.SensorAllFramesReady;
+
+                // Turn on the color stream to receive color frames
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                // Allocate space to put the pixels we'll receive
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                //this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's SkeletonFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            // Color
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == CameraMode.Color)
+                    {
+                        // Copy the pixel data from the image to a temporary array
+                        frame.CopyPixelDataTo(this.colorPixels);
+
+                        // Write the pixel data into our bitmap
+                        this.colorBitmap.WritePixels(
+                            new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                            this.colorPixels,
+                            this.colorBitmap.PixelWidth * sizeof(int),
+                            0);
+                        camera.Source = colorBitmap;
+                    }
+                }
+            }
+
+            // Depth
+            /*using (var frame = e.OpenDepthImageFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == CameraMode.Depth)
+                    {
+                        // Copy the pixel data from the image to a temporary array
+                        frame.CopyPixelDataTo(this.colorPixels);
+
+                        // Write the pixel data into our bitmap
+                        this.colorBitmap.WritePixels(
+                            new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                            this.colorPixels,
+                            this.colorBitmap.PixelWidth * sizeof(int),
+                            0);
+                        camera.Source = colorBitmap;
+                    }
+                }
+            }*/
+
+            // Body
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+                    canvas.Children.Clear();
+
+                    frame.CopySkeletonDataTo(_bodies);
+
+                    foreach (var body in _bodies)
+                    {
+                        if (body.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            // COORDINATE MAPPING
+                            foreach (Joint joint in body.Joints)
+                            {
+                                // 3D coordinates in meters
+                                SkeletonPoint skeletonPoint = joint.Position;
+
+                                // 2D coordinates in pixels
+                                Point point = new Point();
+
+                                if (_mode == CameraMode.Color)
+                                {
+                                    // Skeleton-to-Color mapping
+                                    ColorImagePoint colorPoint = sensor.CoordinateMapper.MapSkeletonPointToColorPoint(skeletonPoint, ColorImageFormat.RgbResolution640x480Fps30);
+
+                                    point.X = colorPoint.X;
+                                    point.Y = colorPoint.Y;
+                                }
+                                /*else if (_mode == CameraMode.Depth) // Remember to change the Image and Canvas size to 320x240.
+                                {
+                                    // Skeleton-to-Depth mapping
+                                    DepthImagePoint depthPoint = sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skeletonPoint, DepthImageFormat.Resolution320x240Fps30);
+
+                                    point.X = depthPoint.X;
+                                    point.Y = depthPoint.Y;
+                                }*/
+
+                                // DRAWING...
+                                Ellipse ellipse = new Ellipse
+                                {
+                                    Fill = Brushes.LightBlue,
+                                    Width = 20,
+                                    Height = 20
+                                };
+
+                                Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
+                                Canvas.SetTop(ellipse, point.Y - ellipse.Height / 2);
+
+                                canvas.Children.Add(ellipse);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DrawPoint(ColorImagePoint point)
+        {
+            // Create an ellipse.
+            Ellipse ellipse = new Ellipse
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Red
+            };
+
+            // Position the ellipse according to the point's coordinates.
+            Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
+            Canvas.SetTop(ellipse, point.Y - ellipse.Height / 2);
+
+            // Add the ellipse to the canvas.
+            canvas.Children.Add(ellipse);
         }
     }
 }
